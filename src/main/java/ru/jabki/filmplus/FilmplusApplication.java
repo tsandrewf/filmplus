f31b072fecca62c1c1535c9fd637b5e1c258c497
tsandrewf <tsandrewf@gmail.com>
2025-10-25 09:06:29 +0300

    init

---
+++ src/main/java/ru/jabki/filmplus/FilmplusApplication.java
@@ -0,0 +1,13 @@
+package ru.jabki.filmplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jabki/filmplus/FilmplusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jabki.filmplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
